e8b4a0e1c1922ec6a6ad4b7390105cc4aa456a2e
Rodrigo Oliveira da Silva <rodrigoods49@gmail.com>
2022-01-13 23:26:31 -0300

    Fixture para a realização de testes, e o Layout que irá servir de base para as páginas web criados

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from garagem.models import Tipo, Proprietario, Veiculo, VagaVeiculo
+
+admin.site.register(Tipo)
+admin.site.register(Proprietario)
+admin.site.register(Veiculo)
+admin.site.register(VagaVeiculo)
--- garagem/urls.py
+++ garagem/urls.py
@@ -2,5 +2,4 @@ from django.urls import path
 from garagem import views
 
 urlpatterns = [
-
 ]
