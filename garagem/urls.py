287c1a0ff275f6e3827d44b9dea76d14f7720788
Rodrigo Oliveira da Silva <rodrigoods49@gmail.com>
2022-01-14 19:56:35 -0300

    Criação e configurações básicas da Página principal, número de registros de cada model e arquivo css criados

--- garagem/urls.py
+++ garagem/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from garagem import views
 
 urlpatterns = [
+    path('', views.index, name='index')
 ]
--- garagem/views.py
+++ garagem/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from garagem.models import Tipo, Proprietario, Veiculo, VagaVeiculo
+
+
+def index(request):
+    num_veiculos = Veiculo.objects.all().count()
+    num_vagas = VagaVeiculo.objects.all().count()
+    num_tipos = Tipo.objects.all().count()
+    num_vagas_ocupadas = VagaVeiculo.objects.filter(situacao__exact='o').count()
+    num_vagas_manutencao = VagaVeiculo.objects.filter(situacao__exact='m').count()
+    num_proprietarios = Proprietario.objects.all().count()
+
+    contexto = {
+        'num_veiculos': num_veiculos,
+        'num_vagas': num_vagas,
+        'num_tipos': num_tipos,
+        'num_vagas_ocupadas': num_vagas_ocupadas,
+        'num_vagas_manutencao': num_vagas_manutencao,
+        'num_proprietarios': num_proprietarios
+    }
+
+    return render(request, 'index.html', context=contexto)
